a7ff8d50cfd376678efcd98fad77c5281e5670c2
vab9 <vbrinkhege@uos.de>
2016-07-18 15:22:32 +0200

    added collatz.rs

--- task2/victor_brinkhege/collatz.rs
+++ task2/victor_brinkhege/collatz.rs
@@ -1,3 +1,21 @@
 fn main() {
     // collatzen
+    for i in 1..21 {
+        println!("{:?} -> {:?}", i, collatz(i));
+    }
+}
+
+fn collatz(mut x: i32) -> i32 {
+    let mut count = 0;
+
+    while x > 1 {
+        count += 1;
+        x = if x % 2 == 0 {
+            x / 2
+        } else {
+            x * 3 + 1
+        }
+    }
+
+    count
 }
