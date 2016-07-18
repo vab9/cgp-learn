c381b3a0f7551002fc9c7447a23ec591933fee48
lepieper <lepieper@uni-osnabrueck.de>
2016-07-18 16:04:03 +0200

    add primes.rs

---
+++ primes.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut z = 1;
+    let mut counter = 2;
+    while z < 21 {
+        if is_prime(counter) == true {
+            println!("{}. Primzahl: {}", z, counter);
+            z = z + 1;
+        } else {}
+        counter = counter + 1;
+    }
+}
+
+fn is_prime(u: u32) -> bool {
+    let mut prime = true;
+
+    for i in 2..u {
+        if u % i == 0 {
+            prime = false;
+        }
+    }
+    prime
+}
