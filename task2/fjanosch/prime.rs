2263ca6aa538af30c6f2e9914f30c2961f120314
FlorianJanosch <fjanosch@uni-osnabrueck.de>
2016-07-18 15:51:24 +0200

    add primes.rs

---
+++ task2/fjanosch/prime.rs
@@ -0,0 +1,19 @@
+fn main() {
+    for i in 1..21 {
+        let a = is_prime(i);
+        if a.1 == true {
+            println!("{} is prime!", a.0)
+        }
+    }
+}
+
+fn is_prime(input: i32) -> (i32, bool) {
+
+    let mut rly_prime = true;
+    for i in 2..input {
+        if (input % i == 0) {
+            rly_prime = false;
+        }
+    }
+    (input, rly_prime)
+}
