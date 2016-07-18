140a03c23670824d66907c076bf5338997447679
vab9 <vbrinkhege@uos.de>
2016-07-18 15:53:36 +0200

    prime

--- task2/victor_brinkhege/prime.rs
+++ task2/victor_brinkhege/prime.rs
@@ -1,3 +1,36 @@
 fn main() {
-    // prime
+    let mut primes = 0;
+
+    for i in 2.. {
+        let result = prime(i);
+
+        if result {
+            println!("{:?} -> {:?}", primes + 1, i);
+            primes += 1;
+        }
+        if primes == 20 {
+            return;
+        }
+    }
+
+}
+
+fn prime(x: i32) -> bool {
+
+    if x % 2 == 0 || x % 3 == 0 {
+        return false;
+    }
+
+    let mut i = 5;
+
+    let limit = (x as f64).sqrt() as i32;
+
+    while i < limit {
+        if x % i == 0 {
+            return false;
+        }
+        i += 2;
+    }
+    return true;
+
 }
