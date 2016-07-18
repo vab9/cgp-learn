bf322aa370b5ff487d124ef18cd4aeda9184bdce
FischerTo <tofischer@uos.de>
2016-07-18 16:09:56 +0200

    rustify the code in prime.rs

--- task2/tobias_fischer/prime.rs
+++ task2/tobias_fischer/prime.rs
@@ -1,31 +1,31 @@
 fn main() {
 
-let mut x = 1;
-while x < 21{
-    println!("{} is prime: {}", x, is_prime(x));
-    x = x + 1;
-}
+    let mut x = 1;
+    while x < 21 {
+        println!("{} is prime: {}", x, is_prime(x));
+        x = x + 1;
+    }
 
 }
 
-fn is_prime(n : u32) -> bool{
-    if n <= 1{
-       return false
-    }
-    else if n <= 3{
-        return true
-    }
-    else if n % 2 == 0 || n % 3 == 0{
-        return false
-    }
-    let mut i = 5;
-    while i*i <= n{
-        if n % i == 0 || n % (i + 2) == 0 {
-            return false
+fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        false
+    } else if n <= 3 {
+        true
+    } else if n % 2 == 0 || n % 3 == 0 {
+        false
+    } else {
+        let mut i = 5;
+        while i * i <= n {
+            if n % i == 0 || n % (i + 2) == 0 {
+                return false;
 
+            }
+            i = i + 6
         }
-        i = i + 6
+
+        true
     }
-    return true
 
 }
