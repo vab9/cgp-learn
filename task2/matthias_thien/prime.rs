24810c9e8f24e0452d6564ea9c8d7dd180ee77ce
MatthiasThien <matthien@cip77.informatik.uni-osnabrueck.de>
2016-07-18 16:20:57 +0200

    Upload collatz.rs and prime.rs

---
+++ task2/matthias_thien/prime.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut n = 0;
+    let mut i = 0;
+    while i < 21 {
+        if is_prime(n) == true {
+            println!("{}", n);
+            i += 1;
+        }
+        n += 1;
+    }
+}
+fn is_prime(n: i32) -> bool {
+    let mut isprime: bool = true;
+
+    if n > 0 && n < 4 {
+        true
+    } else if n == 4 || n == 0 {
+        false
+    } else {
+        for i in 2..n / 2 {
+            if n % i == 0 {
+                isprime = false;
+                break;
+            }
+        }
+        isprime
+    }
+
+}
