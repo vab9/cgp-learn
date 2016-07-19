d30ce00248ab7f6f5c8ac33305d4f71becb81440
Roman Schott <rschott@uni-osnabrueck.de>
2016-07-19 12:18:12 +0200

    deleted redundant to_string() in time.rs

--- task3/rschott/time.rs
+++ task3/rschott/time.rs
@@ -10,7 +10,7 @@ fn clock(n: u8) -> String {
     match n {
         8...12 => "Guten Morgen".to_string(),
         18...22 => "Guten Abend".to_string(),
-        0...5 => format!("Warum bist du denn um {} noch wach", n).to_string(),
+        0...5 => format!("Warum bist du denn um {} noch wach", n),
         _ => "Hallo".to_string(),
     }
 }
