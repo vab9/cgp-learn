0fb1c14f6d5e6359519ee4548ae5525c02ae8fd4
FischerTo <tofischer@uos.de>
2016-07-19 09:36:56 +0200

    change code according to suggestions

--- task2/tobias_fischer/collatz.rs
+++ task2/tobias_fischer/collatz.rs
@@ -1,27 +1,24 @@
 fn main() {
 
-    let mut n = 1;
-    while n < 21 {
+
+    for n in 1..21 {
         println!("N = {} Iterationen {}", n, collatz(n));
-        n = n+1;
     }
 
 
 
 }
 
-fn collatz(mut x: u32) -> (u32){
+fn collatz(mut x: u32) -> (u32) {
     let mut i = 0;
 
-    while x!= 1 {
-        x =
-            if x % 2 == 0{
-                x / 2
-            }
-            else{
-                3 * x +1
-            };
-            i = i + 1;
+    while x != 1 {
+        x = if x % 2 == 0 {
+            x / 2
+        } else {
+            3 * x + 1
+        };
+        i += 1;
     }
     i
 }
