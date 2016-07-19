263a971b152bd5f0cc4eb055862aa99fa6477325
Patrick Nolte <pnolte@uos.de>
2016-07-19 14:12:10 +0200

    fixed eratosthenes

--- task3/patrick_nolte/sieve.rs
+++ task3/patrick_nolte/sieve.rs
@@ -4,16 +4,22 @@ fn main() {
 
 fn eratosthenes(n: u32) -> Vec<u32> {
     let mut v = Vec::new();
-    for x in 1..n {
-        v.push(x);
+    for i in 1..n {
+        v.push(true);
     }
-    let mut count = 0;
-    while count < v.len() {
-        if v[count as usize] % 2 == 0 {
-            v.remove(count);
-        } else {
-            count += 1;
+    for i in 1..n - 1 {
+        for j in 2..i - 1 {
+            if i % j == 0 {
+                v[(i) as usize] = false;
+                continue;
+            }
         }
     }
-    v
+    let mut result = Vec::new();
+    for i in 0..(v.len() - 1) {
+        if v[i as usize] {
+            result.push(i as u32);
+        }
+    }
+    result
 }
