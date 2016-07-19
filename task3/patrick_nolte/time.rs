faaea951741ba1031009ac3d7ea7bcd61f17e55f
Patrick Nolte <pnolte@uos.de>
2016-07-19 14:38:03 +0200

    to_string fixed

--- task3/patrick_nolte/time.rs
+++ task3/patrick_nolte/time.rs
@@ -6,7 +6,7 @@ fn get_message_at_time(time: u8) -> String {
     match time {
         8...12 => "Guten Morgen".to_string(),
         18...22 => "Guten Abend".to_string(),
-        0...5 => format!("Warum bist du denn um {} Uhr noch wach?", time).to_string(),
+        0...5 => format!("Warum bist du denn um {} Uhr noch wach?", time),
         _ => "Hallo".to_string(),
     }
 }
