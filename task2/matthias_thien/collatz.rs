4ba67de25f18d834a8d2c5f712d99135792da8d2
MatthiasThien <matthien@cip77.informatik.uni-osnabrueck.de>
2016-07-19 09:47:36 +0200

    Comments fixed

--- task2/matthias_thien/collatz.rs
+++ task2/matthias_thien/collatz.rs
@@ -1,18 +1,18 @@
-fn collatz(mut n: i32)->i32{
+fn collatz(mut n: i32) -> i32 {
     let mut x = 0;
     while n > 1 {
         n = if n % 2 == 0 {
-            n/2
+            n / 2
         } else {
-            3*n+1
+            3 * n + 1
         };
-        x+=1;
+        x += 1;
     }
     x
 }
 
-fn main(){
+fn main() {
     for i in 1..21 {
-       println!("{} -> {}",i,collatz(i));
+        println!("{} -> {}", i, collatz(i));
     }
 }
