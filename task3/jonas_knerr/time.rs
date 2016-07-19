370f9ce586bbda837801cade1ddaa41e3706b288
Jonas Knerr <jkner@uni-osnabrueck.de>
2016-07-19 11:30:55 +0200

    now with fomrat!

--- task3/jonas_knerr/time.rs
+++ task3/jonas_knerr/time.rs
@@ -3,15 +3,10 @@ fn main() {
     println!("{}", get_time(a));
 }
 fn get_time(time: u8) -> String {
-    let s: String = time.to_string();
-    let mut b: String = "Warum bist du um ".to_string();
-    let c: String = " Uhr noch wach".to_string();
-    b.push_str(&s);
-    b.push_str(&c);
     match time {
         8...12 => "Guten Morgen".to_string(),
         18...22 => "Guten Abend".to_string(),
-        0...5 => b,
+        0...5 => format!("Warum bist du um {} Uhr noch wach?", time),
         _ => "Hallo".to_string(),
     }
 }
