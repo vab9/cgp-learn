3f38decea61c16f9f86b0985ef0ed78d3747492a
FlorianJanosch <fjanosch@uni-osnabrueck.de>
2016-07-19 11:52:58 +0200

    add functionality of Task 3.2 b to time.rs
    changing return-value type to String
    using .to_string()

--- task3/time.rs
+++ task3/time.rs
@@ -1,48 +1,30 @@
 fn main() {
-    println!("{:?}", timely_greeting_if(0));
+    println!("{:?}", timely_greeting_match(4));
     println!("{:?}", timely_greeting_match(8));
-    println!("{:?}", timely_greeting_if(12));
     println!("{:?}", timely_greeting_match(18));
-    println!("{:?}", timely_greeting_if(22));
     println!("{:?}", timely_greeting_match(23));
 
 }
 
-// fn timelygreeting(x: u8) {
-//     let time = if x >= 8 && x <= 12 {
-//         "morning"
+// /// Alte Variante mit if
+// fn timely_greeting_if(x: u8) -> &'static str {
+//     // static Vorschlag von Compiler
+//     if x >= 8 && x <= 12 {
+//         "Guten Morgen"
 //     } else if x >= 18 && x <= 22 {
-//         "evening"
+//         "Guten Abend"
 //     } else {
-//         "allday"
-//     };
-
-//     let greeting = match time {
-//         morning => "Guten Morgen",
-//         evening => "Guten Abend",
-//         allday => "Hallo",
-//     };
-
-//     println!("{}", greeting);
+//         "Hallo"
+//     }
 // }
 
-/// Variante mit if
-fn timely_greeting_if(x: u8) -> &'static str {
-    // static Vorschlag von Compiler
-    if x >= 8 && x <= 12 {
-        "Guten Morgen"
-    } else if x >= 18 && x <= 22 {
-        "Guten Abend"
-    } else {
-        "Hallo"
-    }
-}
-
 /// Variante mit match
-fn timely_greeting_match(x: u8) -> &'static str {
+fn timely_greeting_match(x: u8) -> String {
     match x {
-        8...12 => "Guten Morgen",
-        18...22 => "Guten Abend",
-        _ => "Hallo",//deault
+        // 8...12 => "Guten Morgen"::to_string(&self), //Frage: Wie genau würde das gehen?
+        8...12 => "Guten Morgen".to_string(),
+        18...22 => "Guten Abend".to_string(),
+        0...5 => format!("Warum bist du denn um {X} Uhr noch wach?", X = x),
+        _ => "Hallo".to_string(),//deault
     }
 }
