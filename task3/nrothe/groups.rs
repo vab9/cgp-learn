fdc964797c2af3d28a017fb12f62f7d4fa7bae63
NiklasPaulRothe <nrothe@uni-osnabrueck.de>
2016-07-19 11:53:59 +0200

    autoformat used (hopefully)

--- task3/nrothe/groups.rs
+++ task3/nrothe/groups.rs
@@ -1,16 +1,20 @@
-fn main(){
+fn main() {
     println!("{}", group_letter(Game::Plantex));
     println!("{}", group_letter(Game::SpaceGame));
     println!("{}", group_letter(Game::AVZRun));
 }
 
-fn group_letter(name: Game) -> char{
+fn group_letter(name: Game) -> char {
 
-       match name{
-            Game::AVZRun => 'A',
-            Game::SpaceGame  => 'B',
-            Game::Plantex => 'C',
-       }
+    match name {
+        Game::AVZRun => 'A',
+        Game::SpaceGame => 'B',
+        Game::Plantex => 'C',
+    }
 }
 
-enum Game{AVZRun, SpaceGame, Plantex}
+enum Game {
+    AVZRun,
+    SpaceGame,
+    Plantex,
+}
