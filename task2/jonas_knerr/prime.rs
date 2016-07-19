2ff420becb894c0042b85a990f341a0cd824a260
Jonas Knerr <jkner@uni-osnabrueck.de>
2016-07-19 10:00:04 +0200

    fixed unnesecary boolean

--- task2/jonas_knerr/prime.rs
+++ task2/jonas_knerr/prime.rs
@@ -13,12 +13,12 @@ fn main() {
     }
 }
 fn is_prime(n: i32) -> bool {
-    let isprime = true;
+
     for i in 2..n / 2 + 1 {
         if n % i == 0 {
             return false;
         }
     }
-    isprime
+    true
 
 }
