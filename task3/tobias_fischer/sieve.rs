2142bc1bd582f7b56300eb2dc6a36ffefcb0804f
FischerTo <tofischer@uos.de>
2016-07-19 12:03:59 +0200

    add sieve.rs

---
+++ task3/tobias_fischer/sieve.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let vec = sieve(100);
+    for i in 0..vec.len() {
+        println!("{}", vec[i]);
+    }
+}
+
+fn sieve(n: u32) -> Vec<u32> {
+    let mut v = Vec::new();
+
+    for i in 2..n {
+        v.push(i);
+    }
+    for t in 2..n / 2 {
+        for i in 0..v.len() {
+            if v[i] % t == 0 && t != v[i] {
+                v[i] = 0;   //value = 0 heißt rausgestrichen
+            }
+        }
+    }
+    let mut sol = Vec::new();
+    for i in 0..v.len() {
+        if v[i] != 0 {
+            sol.push(v[i]);
+        }
+    }
+    sol
+}
