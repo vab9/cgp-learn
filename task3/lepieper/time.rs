7584ee4cfbf9c38eb55438d1eb2c29772a1040a3
lepieper <lepieper@uni-osnabrueck.de>
2016-07-19 11:49:38 +0200

    add b) to time.rs

--- task3/lepieper/time.rs
+++ task3/lepieper/time.rs
@@ -4,14 +4,17 @@ fn main() {
     println!("{}", greetings(15));
     println!("{}", greetings(18));
     println!("{}", greetings(20));
+    println!("{}", greetings(3));
 }
 
-fn greetings(t: u8) -> &'static str {
+fn greetings(t: u8) -> String {
     if t >= 8 && t <= 12 {
-        "Guten Morgen"
+        "Guten Morgen".to_string()
     } else if t >= 18 && t <= 22 {
-        "Guten Abend"
+        "Guten Abend".to_string()
+    } else if t <= 5 {
+        format!("Warum bist du denn um {} Uhr noch wach?", t)
     } else {
-        "Hallo"
+        "Hallo".to_string()
     }
 }
