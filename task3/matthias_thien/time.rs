409d43e9bf120cca4fc358a37b39600c067e693d
MatthiasThien <matthien@cip77.informatik.uni-osnabrueck.de>
2016-07-19 12:11:45 +0200

    added task b to time.rs

--- task3/matthias_thien/time.rs
+++ task3/matthias_thien/time.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let time: [u8; 3] = [5, 9, 19];
+    let time: [u8; 4] = [3, 7, 9, 19];
     println!("{}", message(time[0]));
     println!("{}", message(time[1]));
     println!("{}", message(time[2]));
+    println!("{}", message(time[3]));
 }
 fn message(input: u8) -> String {
     match input {
+        0...5 => format!("Warum bist du um {} Uhr noch wach?", input),
         8...12 => "Guten Morgen".to_string(),
         18...22 => "Guten Abend".to_string(),
         _ => "Hallo".to_string(),
