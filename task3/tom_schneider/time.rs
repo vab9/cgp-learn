3e3f6f3518915c38678de028d50ec9b8782a9d91
toschneider <toschneider@cip80.informatik.uni-osnabrueck.de>
2016-07-19 12:25:01 +0200

    fix time 2

--- task3/tom_schneider/time.rs
+++ task3/tom_schneider/time.rs
@@ -11,9 +11,9 @@ fn main() {
 
 fn greet(time: u8) -> String {
     match time {
-        0..5 => format!("Warum bist du denn um {} Uhr noch wach?", time),
-        8..12 => "Guten Morgen".to_string(),
-        18..22 => "Guten Abend".to_string(),
-        _ => "Hallo".to_string()
-
+        0...5 => format!("Warum bist du denn um {} Uhr noch wach?", time),
+        8...12 => "Guten Morgen".to_string(),
+        18...22 => "Guten Abend".to_string(),
+        _ => "Hallo".to_string(),
+    }
 }
