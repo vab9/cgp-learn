d2cb594472cb726f088609f5942736395a687998
FlorianJanosch <fjanosch@uni-osnabrueck.de>
2016-07-19 09:44:30 +0200

    change the prime.rs and do the recommended improvements
    change the return value from a tupel of (i32, bool) to just a bool
    improvements of smaller code like the use of "+="

--- task2/fjanosch/prime.rs
+++ task2/fjanosch/prime.rs
@@ -2,21 +2,20 @@ fn main() {
     let mut count = 20;
     let mut iter = 0;
     while count > 0 {
-        iter = iter + 1;
+        iter += 1;
         let a = is_prime(iter);
-        if a.1 == true {
+        if a == true {
             count = count - 1;
-            println!("{} is prime!", a.0)
+            println!("{} is prime!", iter);
         }
     }
 }
 
-fn is_prime(input: i32) -> (i32, bool) {
+fn is_prime(input: i32) -> (bool) {
     let mut rly_prime = true;
     for i in 2..input {
         if (input % i == 0) {
-            rly_prime = false;
+            return false;
         }
     }
-    (input, rly_prime)
 }
