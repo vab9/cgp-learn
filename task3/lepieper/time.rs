77807c43f0f36a1bfe67f15a5741f3acc328bffd
lepieper <lepieper@uni-osnabrueck.de>
2016-07-19 11:59:53 +0200

    fix if/else to match

--- task3/lepieper/time.rs
+++ task3/lepieper/time.rs
@@ -6,15 +6,11 @@ fn main() {
     println!("{}", greetings(20));
     println!("{}", greetings(3));
 }
-
 fn greetings(t: u8) -> String {
-    if t >= 8 && t <= 12 {
-        "Guten Morgen".to_string()
-    } else if t >= 18 && t <= 22 {
-        "Guten Abend".to_string()
-    } else if t <= 5 {
-        format!("Warum bist du denn um {} Uhr noch wach?", t)
-    } else {
-        "Hallo".to_string()
+    match t {
+        8...12 => "Guten Morgen".to_string(),
+        18...22 => "Guten Abend".to_string(),
+        0...5 => format!("Warum bist du denn um {} Uhr noch wach?", t),
+        _ => "Hallo".to_string(),
     }
 }
