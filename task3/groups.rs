f43fd882e01b16518a9490366e59147a2031dc33
Helena Keller <helenakeller92@gmail.com>
2016-07-19 12:19:52 +0200

    Correction of group and the second task of time

--- task3/groups.rs
+++ task3/groups.rs
@@ -1,19 +1,18 @@
-fn group_letter(name: &str) -> char {
+fn group_letter(name: &str) -> Option<char> {
 
-	match name {
-		"Plantex" => 'C',
-		"AVZ-Run" => 'A',
-		"Space Game" => 'B',
-		_ => '/',
-	}
+    match name {
+        "Plantex" => Some('C'),
+        "AVZ-Run" => Some('A'),
+        "Space Game" => Some('B'),
+        _ => None,
+    }
 }
 
 fn main() {
 
-	println!("AVZ-Run ist Gruppe {}.", group_letter("AVZ-Run"));
-	println!("Space Game ist Gruppe {}.", group_letter("Space Game"));
-	println!("Plantex ist Gruppe {}.", group_letter("Plantex"));
-	println!("Hello ist Gruppe {}.", group_letter("Hello"));
+    println!("AVZ-Run ist Gruppe {:?}.", group_letter("AVZ-Run"));
+    println!("Space Game ist Gruppe {:?}.", group_letter("Space Game"));
+    println!("Plantex ist Gruppe {:?}.", group_letter("Plantex"));
+    println!("Hello ist Gruppe {:?}.", group_letter("Hello"));
 
 }
-
