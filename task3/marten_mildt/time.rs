27de2dee263e00d8369610c5f4f65fb74f6f6e97
Marten Mildt <mmildt@uos.de>
2016-07-19 11:45:24 +0200

    Aufgabe 3.2 b)

--- task3/marten_mildt/time.rs
+++ task3/marten_mildt/time.rs
@@ -2,12 +2,14 @@ fn main() {
     println!("{}", greeting(10));
     println!("{}", greeting(14));
     println!("{}", greeting(20));
+    println!("{}", greeting(2));
 }
 
 fn greeting(hour: u8) -> String {
     match hour {
         8...12 => "Guten Morgen".to_string(),
         18...22 => "Guten Abend".to_string(),
+        0...5 => format!("Warum bist du denn um {} Uhr noch wach?", hour),
         _ => "Hallo".to_string(),
     }
 }
