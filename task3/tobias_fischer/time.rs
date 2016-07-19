bcdf2043fe7fb682e32917cba350a4c0b554625e
FischerTo <tofischer@uos.de>
2016-07-19 11:00:22 +0200

    add time.rs

---
+++ task3/tobias_fischer/time.rs
@@ -0,0 +1,18 @@
+fn main() {
+    for i in 0..23 {
+        println!("{}", greetings(i));
+    }
+}
+
+
+fn greetings(t: u8) -> String {
+    if t <= 5 {
+        format!("Warum bist du denn um {} Uhr noch wach?", t).to_string()
+    } else if t >= 8 && t <= 12 {
+        "Guten Morgen.".to_string()
+    } else if t >= 18 && t <= 22 {
+        "Guten Abend.".to_string()
+    } else {
+        "Hallo".to_string()
+    }
+}
