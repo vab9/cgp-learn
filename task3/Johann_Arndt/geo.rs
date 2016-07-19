0e6d7d085b9cdddf37101e1170038a68e843b703
johann162 <joarndt@uni-osnabrueck.de>
2016-07-19 13:43:27 +0200

    Aufgabe 4

---
+++ task3/Johann_Arndt/geo.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+impl Point {
+    pub fn new() -> Point {
+        Point::origin()
+    }
+    pub fn origin() -> Point {
+        Point { x: 0.0, y: 0.0 }
+    }
+    pub fn is_origin(&self) -> bool {
+        if self.x == 0.0 && self.y == 0.0 {
+            true
+        } else {
+            false
+        }
+    }
+    pub fn distance(&self, p: &Point) -> f32 {
+        let distx = self.x - p.x;
+        let disty = self.y - p.y;
+        (distx * distx + disty * disty).sqrt()
+    }
+}
+
+fn main() {
+
+    let p1 = Point { x: 10.0, y: 10.0 };
+    let p2 = Point::origin();
+    println!("Ist der Punkt im Ursprung? {}", p2.is_origin());
+    println!("Die Distance zwischen {:?} und {:?} ist {}",
+             p1,
+             p2,
+             p1.distance(&p2));
+
+
+}
