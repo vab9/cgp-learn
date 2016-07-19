f47481f852c8ee5a2ac0b20383d005aa6f686d4b
Roman Schott <rschott@uni-osnabrueck.de>
2016-07-19 15:08:00 +0200

    geo.rs, task b

--- task3/rschott/geo.rs
+++ task3/rschott/geo.rs
@@ -3,12 +3,23 @@ fn main() {
     let q = Point::origin();
     println!("{}", p.is_origin());
     println!("{}", q.is_origin());
-    println!("Distance: {}", p.distance(q));
+    println!("Distance: {}", Point::distance(&p, &q));
+
+    let s = Rectangle::new(p, q);
+    println!("Area: {}", s.area());
+    println!("Contains? {}", s.contains(Point::new(5.6, 1.4)));
+    println!("Contains? {}", s.contains(Point::new(1.2, 3.0)));
+
 }
 
 struct Point {
     x: f32,
     y: f32,
+}
+
+struct Rectangle {
+    a: Point,
+    b: Point,
 }
 
 impl Point {
@@ -25,7 +36,67 @@ impl Point {
             false
         }
     }
-    pub fn distance(&self, q: Point) -> f32 {
-        ((q.x - self.x) * (q.x - self.x) + (q.y - self.y) * (q.y - self.y)).sqrt()
+    pub fn distance(p: &Point, q: &Point) -> f32 {
+        ((q.x - p.x) * (q.x - p.x) + (q.y - p.y) * (q.y - p.y)).sqrt()
+    }
+}
+impl Rectangle {
+    pub fn new(x: Point, y: Point) -> Rectangle {
+        Rectangle { a: x, b: y }
+    }
+    pub fn area(&self) -> f32 {
+        ((self.a.x - self.b.x) * (self.a.y - self.b.y)).abs()
+    }
+    pub fn contains(&self, p: Point) -> bool {
+        if self.a.x > self.b.x {
+            if self.a.y > self.b.y {
+                if self.b.x <= p.x && p.x <= self.a.x && self.b.y <= p.y && p.y <= self.a.y {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if self.b.x <= p.x && p.x <= self.a.x && self.a.y <= p.y && p.y <= self.b.y {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            if self.a.y > self.b.y {
+                if self.a.x <= p.x && p.x <= self.b.x && self.b.y <= p.y && p.y <= self.a.y {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if self.a.x <= p.x && p.x <= self.b.x && self.a.y <= p.y && p.y <= self.b.y {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if self.b.x <= p.x && p.x <= self.a.x && self.b.y <= p.y && p.y <= self.a.y {
+            true
+        } else {
+            false
+        }
     }
 }
