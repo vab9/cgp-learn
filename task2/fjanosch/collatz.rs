4f825ae99b86b26709080969816d529ec5554b86
FlorianJanosch <fjanosch@uni-osnabrueck.de>
2016-07-19 09:43:06 +0200

    change the code and do the recommended improvement for the if-statement

--- task2/fjanosch/collatz.rs
+++ task2/fjanosch/collatz.rs
@@ -8,11 +8,10 @@ fn collatz(m: i32) -> i32 {
     let mut n = m;
     let mut count = 0;
     while n != 1 {
+        count += 1;
         n = if n % 2 == 0 {
-            count = count + 1;
             n / 2
         } else {
-            count = count + 1;
             n * 3 + 1
         }
     }
