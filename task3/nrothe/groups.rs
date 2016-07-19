099bada976b8538634542177ad3992624df88533
NiklasPaulRothe <nrothe@uni-osnabrueck.de>
2016-07-19 11:28:35 +0200

    changed to enum

--- task3/nrothe/groups.rs
+++ task3/nrothe/groups.rs
@@ -1,15 +1,16 @@
 fn main(){
-    println!("{}", group_letter("Plantex"));
+    println!("{}", group_letter(Game::Plantex));
+    println!("{}", group_letter(Game::SpaceGame));
+    println!("{}", group_letter(Game::AVZRun));
 }
 
-fn group_letter(name: &str) -> char{
+fn group_letter(name: Game) -> char{
 
        match name{
-            "AVZ-Run" => 'A',
-            "Space Game"  => 'B',
-            "Plantex" => 'C',
-            _ => 'X',
+            Game::AVZRun => 'A',
+            Game::SpaceGame  => 'B',
+            Game::Plantex => 'C',
        }
-
-
 }
+
+enum Game{AVZRun, SpaceGame, Plantex}
