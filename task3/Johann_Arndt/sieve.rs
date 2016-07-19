00aa1e6018829646c37284cd6c24afd6d04f4f84
johann162 <joarndt@uni-osnabrueck.de>
2016-07-19 12:12:24 +0200

    Aufgabe 3

---
+++ task3/Johann_Arndt/sieve.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let n = 100;
+    let liste = sieb(&vec![true;n]);
+
+    for i in 0..n {
+        if liste[i] {
+            println!("{}", i);
+        }
+    }
+}
+
+fn sieb(todo: &Vec<bool>) -> Vec<bool> {
+    let mut i = 2;
+    let mut liste = todo.clone();
+    while i > 1 && i + 1 < liste.len() {
+        for j in i + 1..liste.len() {
+            if j % i == 0 {
+                liste[j] = false;
+            }
+        }
+        i += 1;
+    }
+
+    liste
+}
