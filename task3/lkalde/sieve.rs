3a15bc4069f0c00a7a5436f96c0f623f60e5f6ae
Lukas Kalde <lkalde@uos.de>
2016-07-19 14:27:18 +0200

    Fixed sieve.rs

--- task3/lkalde/sieve.rs
+++ task3/lkalde/sieve.rs
@@ -3,7 +3,7 @@
 fn main() {
     let n = 100;
     let prime = sieve(n);
-    for i in 1..prime.len() {
+    for i in 0..prime.len() {
         println!("{} is a prime number", prime[i as usize]);
     }
 
@@ -12,31 +12,27 @@ fn main() {
 
 /// Implement sieve of Eratosthenes
 fn sieve(n: u32) -> Vec<u32> {
-    let mut numbers = Vec::with_capacity(n as usize);   //Hold true for index if possible prime number
+    // Since 0 and 1 are no prime numbers, numbers[i] refers to number i+2.
+
+    // numbers will hold all numbers from 2 to n
+    let mut numbers = vec![true; (n-1) as usize];
 
     // calculate upper boarder to which we need to count in the sieve
-    let b = (n as f32).sqrt().ceil() as u32;
-
-    // assume all numbers as prime numbers
-    for _ in 0..n + 1 {
-        numbers.push(true);
-    }
-    numbers[1] = false;  //by definition
-
+    let b = ((n - 2) as f32).sqrt().ceil() as u32;
 
 
     let mut start;  //initial value for sieve iteration
     let mut j;      //current number that is to be scratched
 
     // execute actual sieve
-    for i in 2..b + 1 {
+    for i in 0..b + 1 {
         // only need to scratch multiple of i if i has not been scratched yet
         if numbers[i as usize] == true {
-            start = i;
-            j = 2 * i;
+            start = i + 2;
+            j = 2 * start;
             // scracth all multiples
             while j <= n {
-                numbers[j as usize] = false;
+                numbers[(j - 2) as usize] = false;
                 j += start;
             }
         }
@@ -44,11 +40,12 @@ fn sieve(n: u32) -> Vec<u32> {
 
 
 
+
     // prepare return vector
     let mut ret = Vec::new();
-    for i in 0..n + 1 {
-        if numbers[i as usize] == true {
-            ret.push(i);
+    for i in 0..(n - 1) {
+        if numbers[i as usize] {
+            ret.push(i + 2);
         }
     }
 
