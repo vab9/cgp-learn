4d24bc319cc6470d78a117ed44805ee7c5a48caf
vab9 <vbrinkhege@uos.de>
2016-07-19 13:26:17 +0200

    finished time.rs

--- task3/victor_brinkhege/time.rs
+++ task3/victor_brinkhege/time.rs
@@ -1,8 +1,13 @@
-fn time(usize) -> &str {
-
+fn time(t: u8) -> String {
+    match t {
+        0...5 => format!{"Warum bist du um {} noch wach?", t},
+        8...12 => "Guten Morgen".to_string(),
+        18...22 => "Guten Abend".to_string(),
+        _ => "Hallo".to_string(),
+    }
 
 }
 
 fn main() {
-    println!("3 Uhr {}", time(3));
+    println!("3 Uhr: {}", time(3));
 }
