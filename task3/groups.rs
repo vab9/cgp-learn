3232420531ef5f9e73f6061727013ded984c6f75
FlorianJanosch <fjanosch@uni-osnabrueck.de>
2016-07-19 10:52:30 +0200

    add groups.rs (Task 3.1)

---
+++ task3/groups.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("{:?}", group_letter(Group::AvzRun));
+    println!("{:?}", group_letter(Group::SpaceGame));
+    println!("{:?}", group_letter(Group::Plantex));
+    // println!("{:?}", group_letter("Plantex"));
+}
+
+enum Group {
+    Plantex,
+    AvzRun,
+    SpaceGame,
+}
+fn group_letter(x: Group) -> char {
+    let letter = match x {
+        Group::Plantex => 'c',
+        Group::AvzRun => 'a',
+        Group::SpaceGame => 'b',
+    };
+    letter
+}
