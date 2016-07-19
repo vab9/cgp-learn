a0ba68d7d76956e6dc4f6a0c2cc59bf0f0f8df46
FischerTo <tofischer@uos.de>
2016-07-19 11:25:16 +0200

    change if else to match

--- task3/tobias_fischer/time.rs
+++ task3/tobias_fischer/time.rs
@@ -6,13 +6,10 @@ fn main() {
 }
 
 fn greetings(t: u8) -> String {
-    if t <= 5 {
-        format!("Warum bist du denn um {} Uhr noch wach?", t).to_string()
-    } else if t >= 8 && t <= 12 {
-        "Guten Morgen.".to_string()
-    } else if t >= 18 && t <= 22 {
-        "Guten Abend.".to_string()
-    } else {
-        "Hallo".to_string()
+    match t {
+        0...5 => format!("Warum bist du denn um {} Uhr noch wach?", t).to_string(),
+        8...12 => "Guten Morgen.".to_string(),
+        18...22 => "Guten Abend.".to_string(),
+        _ => "Hallo".to_string(),
     }
 }
