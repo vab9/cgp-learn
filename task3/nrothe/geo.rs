b8904424eb2b5eed372f61ea50d95799d7bb3212
NiklasPaulRothe <nrothe@uni-osnabrueck.de>
2016-07-19 15:15:56 +0200

    added parts of 4.2

--- task3/nrothe/geo.rs
+++ task3/nrothe/geo.rs
@@ -1,9 +1,11 @@
 fn main() {
     let p = Point::origin();
-    println!("{}", p.is_origin());
-    let q = Point::new(5.0, 0.0);
-    println!("{}", q.is_origin());
-    println!("{}", q.distance(p));
+    println!("{}", &p.is_origin());
+    let q = Point::new(5.0, 5.0);
+    println!("{}", &q.is_origin());
+    println!("{}", &q.distance(&p));
+
+    let r = Rectangle::new(p, q);
 }
 
 struct Point {
@@ -21,17 +23,32 @@ impl Point {
     }
 
     pub fn is_origin(&self) -> bool {
-        if self.x == 0.0 && self.y == 0.0
+        self.x == 0.0 && self.y == 0.0
     }
 
-    pub fn distance(&self, p: Point) -> f32 {
+    pub fn distance(&self, p: &Point) -> f32 {
         (((self.x - p.x) * (self.x - p.x)) + ((self.y - p.y) * (self.y - p.y))).sqrt()
     }
 }
 
-struct Rectangle{
-    p1: f32,
-    p2: f32,
+struct Rectangle {
+    p1: Point,
+    p2: Point,
     length: f32,
     width: f32,
+}
+
+impl Rectangle {
+    pub fn new(p_one: Point, p_two: Point) -> Rectangle {
+        Rectangle {
+            length: (&p_one.x - &p_two.x),
+            width: (&p_one.y - &p_two.y),
+            p1: p_one,
+            p2: p_two,
+        }
+    }
+
+    pub fn area(&self) -> f32 {
+        (self.length * self.width).abs()
+    }
 }
