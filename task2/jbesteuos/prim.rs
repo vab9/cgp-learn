2bea1dc446d6aa8e223d9fe8de6e180337adfda8
jbesteuos <jbeste@uos.de>
2016-07-19 10:04:39 +0200

    make jony happy

--- task2/jbesteuos/prim.rs
+++ task2/jbesteuos/prim.rs
@@ -2,19 +2,16 @@ fn main() {
 
     for x in 2..21 {
         println!("{} -> {}", x, prim(x));
+
     }
-
 }
-
 fn prim(n: i32) -> bool {
 
     for teiler in 2..n {
-
         if n % teiler == 0 {
             return false;
-
         }
-    }
 
-    true;
+    }
+    true
 }
