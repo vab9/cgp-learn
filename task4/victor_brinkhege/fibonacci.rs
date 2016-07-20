d5ace8aa6d755fecbc90608bf3ee9c404ee50387
vab9 <vbrinkhege@uos.de>
2016-07-20 11:35:08 +0200

    finish fibonacci.rs

--- task4/victor_brinkhege/fibonacci.rs
+++ task4/victor_brinkhege/fibonacci.rs
@@ -1,10 +1,33 @@
-struct MyIterator {
-
+struct MyType {
+    current: usize,
+    next: usize,
 }
 
+impl MyType {
+    fn new() -> Self {
+        MyType {
+            current: 1,
+            next: 1,
+        }
+    }
+}
+
+impl Iterator for MyType {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        let following = self.current + self.next;
+
+        self.current = self.next;
+        self.next = following;
+
+        Some(self.current)
+
+    }
+}
 
 fn main() {
-    for i in MyIterator::new().take(20) {
+    for i in MyType::new().take(20) {
         println!("{}", i);
     }
 }
